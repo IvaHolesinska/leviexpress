5748b20ad5fe6fe0c6198899d70f55a739479bcc
Iva Holešinská <holesinska.iva@gmail.com>
2021-05-22 14:55:39 +0200

    routing

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
 
 import './style.css';
 import './Header/style.css';
 import './Footer/style.css';
+import Home from './Home/Home';
+import Reservation from './Reservation/Reservation';
 
 const App = () => (
-  <>
+  <Router>
     <header>
       <Header />
     </header>
-    <main></main>
+
+    <Switch>
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="/reservation">
+        <Reservation />
+      </Route>
+    </Switch>
+
     <footer>
       <Footer />
     </footer>
-  </>
+  </Router>
 );
 
 render(<App />, document.querySelector('#app'));
