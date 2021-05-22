29789facffc80f8ed4499b96bc840dc080079572
Iva Holešinská <holesinska.iva@gmail.com>
2021-05-22 15:12:29 +0200

    information about journey

--- src/index.jsx
+++ src/index.jsx
@@ -7,15 +7,17 @@ import Header from './Header/Header';
 import './style.css';
 import './Header/style.css';
 import './Footer/style.css';
+import './Home/JourneyPicker/style.css';
 import Home from './Home/Home';
 import Reservation from './Reservation/Reservation';
+import JourneyPicker from './Home/JourneyPicker/JourneyPicker';
 
 const App = () => (
   <Router>
     <header>
       <Header />
     </header>
-
+    <JourneyPicker />
     <Switch>
       <Route path="/">
         <Home />
