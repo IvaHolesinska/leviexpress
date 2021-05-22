727f1997131989bb9c7ebc88bbbb13b7f93d7ad2
Iva Holešinská <holesinska.iva@gmail.com>
2021-05-22 14:42:31 +0200

    Header and Footer

---
+++ src/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <h1 className="site-title">
+          <a href="/">LeviExpress</a>
+        </h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+
 import './style.css';
+import './Header/style.css';
+import './Footer/style.css';
 
 const App = () => (
-  <div className="container">
+  <>
     <header>
-      <div className="logo"></div>
-      <h1>Webová aplikace</h1>
+      <Header />
     </header>
-    <main>
-      <p>Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>.</p>
-    </main>
+    <main></main>
     <footer>
-      <p>Czechitas, Digitální akademie: Web</p>
+      <Footer />
     </footer>
-  </div>
+  </>
 );
 
 render(<App />, document.querySelector('#app'));
